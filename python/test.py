0dc45f34885e14acc50e6c621db5e16980430c82
adipapp <adipapp98@gmail.com>
2018-11-13 23:08:38 +0100

    Hoppá, ez lemaradt

--- python/test.py
+++ python/test.py
@@ -12,6 +12,6 @@ sensor=sys.argv[1]
 max_id=time*freq
 
 for i in range(1, max_id+1):
-	print(str(datetime.datetime.now()) + '	' + sensor + '	' + str(i))
+	print(str(datetime.datetime.now()) + '#' + sensor + '#' + str(i))
 	sys.stdout.flush()
 	sleep(1/freq)
--- web/geogebra/geogebra.js
+++ web/geogebra/geogebra.js
@@ -1,9 +1,7 @@
 window.onload = function () {
-    var div = document.getElementById("geogebra");
     var ggbApp = new GGBApplet({
-        "appName": "classic", "width": window.screen.availWidth,
-        "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true
-    }, true);
-        ggbApp.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
-        ggbApp.inject('geogebra');
+        "appName": "classic", "width": window.screen.availWidth+100, "height": window.screen.availHeight,
+        "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true }, true);
+    ggbApp.setHTML5Codebase('GeoGebra/HTML5/5.0/web3d/');
+    ggbApp.inject('geogebra');
 }
