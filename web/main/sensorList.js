ed1997b42fa004c9dea5ba37259393808919b66f
adipapp <adipapp98@gmail.com>
2018-11-15 21:54:06 +0100

    Stílus módosítások, custom font-family

--- web/main/sensorList.js
+++ web/main/sensorList.js
@@ -2,7 +2,7 @@
 
 var list = [
     ["Válassz", "", "", ""],
-    ["Hőmérséklet(folyadék)", "../img/sensor_temp_liq.jpg", "../img/sensor_temp_liq_wiring.jpg", "aqtemp.py"],
+    ["Hőmérséklet(folyadék)", "../img/sensor_temp_liq.JPG", "../img/sensor_temp_liq_wiring.jpg", "aqtemp.py"],
     ["Fényerősség", "../img/sensor_light.jpg", "../img/sensor_light_wiring.jpg", "light.py"],
     ["Gyorsulás", "../img/sensor_gyro.jpg", "../img/sensor_gyro_wiring.jpg", "gyro.py"],
     ["Hőmérséklet(gáz)", "../img/sensor_temp.jpg", "../img/sensor_temp_wiring.jpg", "temp.py"]
